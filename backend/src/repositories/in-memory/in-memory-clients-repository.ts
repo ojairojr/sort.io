ad406989222a8e3ff5d091b63779319ceb35f8a2
Jairo Júnior <jairojunior.png@gmail.com>
2024-01-16 18:10:22 -0300

    fix: return function findClientId

--- backend/src/repositories/in-memory/in-memory-clients-repository.ts
+++ backend/src/repositories/in-memory/in-memory-clients-repository.ts
@@ -4,14 +4,15 @@ import { ClientsRepository } from '../clients-repository'
 export class InMemoryClientsRepository implements ClientsRepository {
   public items: Clients[] = []
   async create(clients: Clients): Promise<void> {
+    console.log(this.items)
     this.items.push(clients)
   }
 
   async findClientId(id: string): Promise<null | Clients> {
     const client = this.items.find((client) => client.id === id)
-    if (client) {
-      return client
+    if (!client) {
+      return null
     }
-    return null
+    return client
   }
 }
